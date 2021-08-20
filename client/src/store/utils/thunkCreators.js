f2e21593f7ded863d9b9739f88ccbf4cf1fc91bc
Ryan <ryanzzheng@gmail.com>
2021-08-20 14:07:12 -0400

    Fixed indentation not match

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -73,7 +73,7 @@ export const fetchConversations = () => async (dispatch) => {
   try {
     const { data } = await axios.get("/api/conversations");
     dispatch(gotConversations(data));
-    
+
   } catch (error) {
     console.error(error);
   }
@@ -95,14 +95,14 @@ const sendMessage = (data, body) => {
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
 export const postMessage = (body) => async (dispatch) => {
-  try {    
+  try {
     const data = await saveMessage(body);
-      if (!body.conversationId) {
-        dispatch(addConversation(body.recipientId, data.message));
-      } else {
-        dispatch(setNewMessage(data.message));
-      }
-      sendMessage(data, body);
+    if (!body.conversationId) {
+      dispatch(addConversation(body.recipientId, data.message));
+    } else {
+      dispatch(setNewMessage(data.message));
+    }
+    sendMessage(data, body);
   } catch (error) {
     console.error(error);
   }
