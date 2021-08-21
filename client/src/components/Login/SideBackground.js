7d7028633831ddaed7c011051d88b7ba92ab3423
Ryan <ryanzzheng@gmail.com>
2021-08-21 00:11:43 -0400

    Fixed browser incompatible issue

--- client/src/components/Login/SideBackground.js
+++ client/src/components/Login/SideBackground.js
@@ -14,7 +14,6 @@ const useStyles = makeStyles((theme) => ({
       backgroundSize: 'cover',
       padding: '200px 20px 200px 20px',
       textAlign: 'center',
-      height: '-webkit-fill-available'
     },
     p: {
       fontSize: '20px',
