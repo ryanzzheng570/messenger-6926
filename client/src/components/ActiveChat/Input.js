bf4cb03cc0895d01ef655f2470d07cc09848e5a3
Ryan <ryanzzheng@gmail.com>
2021-08-22 21:06:46 -0400

    Added Input field Icon buttons and onClick handlers
    - Added Cloudinary dependencies
    - Created my own cloudinary account with dashboard named demoryanzzheng

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import { FormControl, FilledInput } from "@material-ui/core";
+import { FormControl, FilledInput, IconButton } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
 import { postMessage } from "../../store/utils/thunkCreators";
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import ImageIcon from '@material-ui/icons/Image';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -14,12 +16,16 @@ const useStyles = makeStyles(() => ({
     backgroundColor: "#F4F6FA",
     borderRadius: 8,
     marginBottom: 20
+  },
+  input_icon: {
+    opacity: 0.7,
   }
 }));
 
 const Input = (props) => {
   const classes = useStyles();
   const [text, setText] = useState("");
+  const [attachments, setAttachments] = useState([]);
   const { postMessage, otherUser, conversationId, user } = props;
 
   const handleChange = (event) => {
@@ -33,13 +39,17 @@ const Input = (props) => {
       text: event.target.text.value,
       recipientId: otherUser.id,
       conversationId,
-      sender: conversationId ? null : user
+      sender: conversationId ? null : user,
+      attachments: [attachments]
     };
 
     await postMessage(reqBody);
     setText("");
-    
   };
+
+  const handleUploadImage = () => {
+
+  }
 
   return (
     <form className={classes.root} onSubmit={handleSubmit}>
@@ -51,6 +61,16 @@ const Input = (props) => {
           value={text}
           name="text"
           onChange={handleChange}
+          endAdornment={
+            <>
+              <IconButton color='secondary'>
+                <InsertEmoticonIcon className={classes.input_icon} />
+              </IconButton>
+              <IconButton color='secondary' onClick={handleUploadImage}>
+                <ImageIcon className={classes.input_icon} />
+              </IconButton>
+            </>
+          }
         />
       </FormControl>
     </form>
--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
-
+import { CloudinaryContext, Image } from 'cloudinary-react'
+import { cloudinaryCloudName } from "./utils/Constants";
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
@@ -13,7 +14,15 @@ const Messages = (props) => {
           const time = moment(message.createdAt).format("h:mm");
 
           return message.senderId === userId ? (
-            <SenderBubble key={message.id} text={message.text} time={time} />
+            <>
+              <SenderBubble key={message.id} text={message.text} time={time} />
+              <CloudinaryContext cloudName={cloudinaryCloudName}>
+                <div>
+                  <Image publicId="sample" width="50" />
+                </div>
+                {/* <Image publicId="sample" width="0.5" /> */}
+              </CloudinaryContext>
+            </>
           ) : (
             <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
           );
